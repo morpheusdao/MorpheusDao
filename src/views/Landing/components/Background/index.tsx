cb933e3c158e7a8eefd9ba202e07aee5bf21e321
Matiyas-H <mattsis71@icloud.com>
2021-12-26 23:19:46 +0200

    removedbg

--- src/views/Landing/components/Background/index.tsx
+++ src/views/Landing/components/Background/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./background.scss";
-import image from "../../../../assets/icons/image.png";
+import BlobsTop from "../../../../assets/icons/image.png";
 
 import BlobsCenter from "../../../../assets/icons/landing-blobs-center.png";
 
@@ -8,7 +8,7 @@ function Background() {
     return (
         <div className="landing-background">
             <div className="landing-background-blobs-top">
-                <img alt="" src={image} />
+                <img alt="" src={BlobsTop} />
             </div>
         </div>
     );
